859e5e18db139b9e4f704ca87fb621e527240b3d
Chaw Su Thwe <chawsuthwe@Chaws-iMac.lan>
2021-06-08 21:12:37 +0630

    homework commit

---
+++ if..else.js
@@ -0,0 +1,27 @@
+var trafficLight = 'yellow';
+
+if (trafficLight == 'red'){
+	console.log('STOP');
+}else if (trafficLight == 'yellow'){
+	console.log('Slow Down');
+}else if (trafficLight == 'green'){
+	console.log('GO');
+}else{
+	console.log('Sorry! Something is wrong.');
+}
+
+
+
+var season = 'summer';
+
+if (season == 'spring'){
+	console.log("It's spring!");
+}else if (season == 'summer'){
+	console.log("It's summer!");	
+}else if (season == 'rainy'){
+	console.log("It's rainy season!");
+}else if (season == 'winter'){
+	console.log("It's winter!");
+}else{
+	console.log("Sorry! Something is wrong.");
+}
---
+++ switch..case.js
@@ -0,0 +1,16 @@
+switch(fruits = "Apple") {
+	case "Apple":
+		console.log('The color of Apple is red.');
+	break;
+	case "Banana":
+		console.log('The color of Banana is yellow.');
+	break;
+	case "Grapes":
+		console.log('The color of Grapes is purple.');
+	break;
+	case "Orange":
+		console.log('The color of Orange is orange.');
+	break;
+	default:
+		console.log('Try Again!');
+}
